ad74bdb2f5a37a2472869b5544dae8e378e25a1e
Mannypulator <emeritus93@gmail.com>
2025-02-01 14:48:23 +0100

    first commit

---
+++ HNG12.API/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.MapGet("/", () =>{
+    var response = new
+    {
+        email = "emeritus93@gmail.com",
+        current_datetime = DateTime.UtcNow.ToString("o"),
+        github_url = "https://github.com/mannypulator/solid-octo-carnival"
+    };
+
+    return Results.Json(response);
+});
+
+app.Run();
